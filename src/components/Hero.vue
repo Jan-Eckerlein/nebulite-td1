b55ee3175e5459f952e378c5c2c1647ef2c0a3c6
Jan Eckerlein <jan.eckerlein@fkt42.de>
2023-06-05 00:12:45 +0200

    top level edit var

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -6,7 +6,7 @@
 
 <template>
 	<section class="w-full bg-gradient-to-br from-violet-600 to-blue-400">
-		<div class="container flex  gap-8 py-32">
+		<div class="container grid grid-cols-2 gap-8 py-32">
 			<slot name="image" />
 			<slot name="text" />
 		</div>
